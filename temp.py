c831ef2cb0d842b736bc673649c05270383d724a
Adam Beigel <31280510+abeige@users.noreply.github.com>
2022-09-24 10:36:12 -0500

    average temp

--- temp.py
+++ temp.py
@@ -1,11 +1,24 @@
-from machine import ADC
+from machine import ADC, Timer
+from time import sleep
+
 temp_sensor = ADC(4)
-temperature = temp_sensor.read_u16()
-
+timer = Timer()
 to_volts = 3.3 / 65535
-temperature = temperature * to_volts
 
-celsius_degrees = 27 - (temperature - 0.706) / 0.001721
-fahrenheit_degrees = celsius_degrees * 9 / 5 + 32
+interval = 5
+temps = []
 
-print(fahrenheit_degrees)
+def read_temp(timer):
+    temp = temp_sensor.read_u16()
+    temp_volts = temp * to_volts
+    celsius_degrees = 27 - (temp_volts - 0.706) / 0.001721
+    fahrenheit_degrees = celsius_degrees * 9 / 5 + 32
+    temps.append(fahrenheit_degrees)
+    print(f"{fahrenheit_degrees:.2f}")
+
+timer.init(freq=5, mode=Timer.PERIODIC, callback=read_temp)
+sleep(interval)
+timer.deinit()
+
+avg = sum(temps) / len(temps)
+print(f"Average temp over {interval} seconds: {avg:.2f}")
