370fd6cca6e96b2f7cd1ff2beaad28d6fc179659
Adam Beigel <31280510+abeige@users.noreply.github.com>
2022-09-24 10:17:17 -0500

    function to stop led flash

--- flash.py
+++ flash.py
@@ -1,8 +1,12 @@
 from machine import Pin, Timer
-led = Pin(15, Pin.OUT)
+led = Pin("LED", Pin.OUT)
 timer = Timer()
 
 def blink(timer):
     led.toggle()
+
+def stop():
+    timer.deinit()
+    led.off()
 
 timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
